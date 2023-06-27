7d4118ded740ad6d8b8e1ab8c75df070acc02339
Oles Leskiv <olesleskiv@Oless-MacBook-Pro.local>
2023-06-27 22:46:12 +0300

    create Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import type { ICard } from '.';
+
+import './Card.scss';
+
+interface Props {
+  flipped?: boolean;
+  disabled?: boolean;
+  onClick: (card: ICard) => void;
+  card: ICard;
+}
+
+export const Card: React.FC<Props> = ({ flipped, disabled, onClick, card }) => {
+  const classes = classNames('card', { 'card--flipped': flipped });
+
+  const handleClick = () => {
+    if (disabled) return;
+    onClick(card);
+  };
+
+  return (
+    <div className={classes}>
+      <div className="front-face">{card.name}</div>
+      <div className="back-face" onClick={handleClick}>
+        🦄
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Card/index.ts
@@ -0,0 +1,2 @@
+export { Card } from './Card';
+export type { ICard } from './types';
