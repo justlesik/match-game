0095f971ca66c09b4d0f602996a9f2297693a629
Oles Leskiv <olesleskiv@Oless-MacBook-Pro.local>
2023-06-27 23:55:25 +0300

    create main layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Board, Difficulty } from '../components/Board';
 
 import './App.scss';
 
-export const App = () => {
-  return <div>App</div>;
+export const App: React.FC = () => {
+  const [difficulty, setDifficulty] = useState(Difficulty.easy);
+  return (
+    <div className="container">
+      <h1>Find mathes</h1>
+      <div>
+        <button onClick={() => setDifficulty(Difficulty.easy)}>Easy</button>
+        <button onClick={() => setDifficulty(Difficulty.normal)}>Normal</button>
+        <button onClick={() => setDifficulty(Difficulty.hard)}>Hard</button>
+      </div>
+      <Board difficulty={difficulty} />
+    </div>
+  );
 };
